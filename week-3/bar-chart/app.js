8c20fae5b6a021b81c6edfaee5c2cf5521410b9e
Rich-Harris <richard.a.harris@gmail.com>
2016-02-08 07:58:02 -0500

    intro effect

--- week-3/bar-chart/app.js
+++ week-3/bar-chart/app.js
@@ -2,15 +2,21 @@
 // it with a `main` variable
 var $main = $( 'main' );
 
-function createBar ( month ) {
+function createBar ( month, i ) {
 	var $container = $( '<div class="container"></div>' );
 
 	var $label     = $( '<span class="small label">' + month.name.slice( 0, 3 ) + '</span>' );
 	var $bigLabel  = $( '<span class="big label">' + month.name + '</span>' );
-	var $bar       = $( '<div class="bar" style="width: ' + month.temp + '%;" data-temp="' + month.temp + '"></div>' );
+	var $bar       = $( '<div class="bar" style="width: 0;" data-temp="' + month.temp + '"></div>' );
 
 	// add the label and bar to the container...
 	$container.append( $label, $bigLabel, $bar );
+
+	$container.fadeIn( 800 );
+
+	setTimeout( function () {
+		$bar.css( 'width', month.temp + '%' );
+	}, i * 100 );
 
 	// ...then add the container to the page
 	$main.append( $container );
@@ -22,5 +28,5 @@ var i;
 // `temperatureByMonth` is defined in data.js
 for ( i = 0; i < temperatureByMonth.length; i += 1 ) {
 	month = temperatureByMonth[i];
-	createBar( month );
+	createBar( month, i );
 }

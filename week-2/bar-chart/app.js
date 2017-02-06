1d9766a7866ea98283ca642dace300a6b4d96435
Rich-Harris <richard.a.harris@gmail.com>
2017-02-06 18:29:11 -0500

    week 3 start

--- week-2/bar-chart/app.js
+++ week-2/bar-chart/app.js
@@ -2,37 +2,31 @@
 // it with a `main` variable
 var main = document.querySelector( 'main' );
 
-var month;
-var i;
-
 function createBar ( month ) {
 	// <div class='container'>...</div>
-	var container = document.createElement( 'div' );
+	var container = document.createElement( 'div' ); 
 	container.setAttribute( 'class', 'container' );
 
-	// <span class='small label'>{text}</span>
+	// <span class='label'>{text}</span>
 	var label = document.createElement( 'span' );
-	label.setAttribute( 'class', 'small label' );
+	label.setAttribute( 'class', 'label' );
 	label.textContent = month.name.slice( 0, 3 );
-
-	// <span class='big label'>{text}</span>
-	var bigLabel = document.createElement( 'span' );
-	bigLabel.setAttribute( 'class', 'big label' );
-	bigLabel.textContent = month.name;
 
 	// <div class='bar' style='width: ${width}%;'></div>
 	var bar = document.createElement( 'div' );
 	bar.setAttribute( 'class', 'bar' );
 	bar.style.width = month.temp + '%';
 
-	// this allows us to create a 'pseudo-element' that
-	// labels the temperature – see the `.bar::after`
-	// block in styles.css
-	bar.setAttribute( 'data-temp', month.temp );
+	// <span class='value'>{value}</span>
+	var value = document.createElement( 'span' );
+	value.setAttribute( 'class', 'value' );
+	value.textContent = month.temp;
 
-	// add the label and bar to the container...
+	// add the value to the bar...
+	bar.appendChild( value );
+
+	// ...then add the label and bar to the container...
 	container.appendChild( label );
-	container.appendChild( bigLabel );
 	container.appendChild( bar );
 
 	// ...then add the container to the page
@@ -40,7 +34,7 @@ function createBar ( month ) {
 }
 
 // `temperatureByMonth` is defined in data.js
-for ( i = 0; i < temperatureByMonth.length; i += 1 ) {
-	month = temperatureByMonth[i];
+for ( var i = 0; i < temperatureByMonth.length; i += 1 ) {
+	var month = temperatureByMonth[i];
 	createBar( month );
 }
